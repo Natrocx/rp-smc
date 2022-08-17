d518b928527ed48ac06d436b3254c1bd185f3de7
Jonas Lauschke <natrocx@protonmail.ch>
2022-08-17 11:00:00 +0200

    Added short actuation

--- src/consts.rs
+++ src/consts.rs
@@ -8,5 +8,12 @@ use embedded_hal::digital::v2::PinState;
 /// otherwise, it will be driven to Ground.
 pub const GPIO_SHORT_ACTUATION_ACTIVE_POLARITY: PinState = PinState::High;
 
+/// The time period the short actuation pin will be toggled to its active state.
+/// Specified in Microseconds - the RP2040 has a 1MHz Timer tick rate
+pub const GPIO_SHORT_ACTUATION_PERIOD: u32 = 500_000;
+
+/// According to the ATX specification, the boot signal must be pulled to ground to activate the PSU
+pub const GPIO_CONTINUOUS_ACTUATION_ACTIVE_POLARITY: PinState = PinState::Low;
+
 /// The rp2040 comes with a 1 MHz Timer, and 64 bits precision. Overflows need not be considered.
 pub const TIMER_FREQUENCY: u64 = 1_000_000;
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,9 @@ use panic_halt as _;
 #[rtic::app(device = rp_pico::hal::pac, peripherals = true)]
 mod app {
 
-  use cortex_m::delay::Delay;
-use defmt::*;
+  use defmt::*;
   use defmt_rtt as _;
-  use embedded_hal::digital::v2::{OutputPin, PinState};
+  use embedded_hal::digital::v2::OutputPin;
   use embedded_time::duration::Extensions;
   use rp_pico::hal::clocks::init_clocks_and_plls;
   use rp_pico::hal::timer::Alarm;
@@ -18,35 +17,34 @@ use defmt::*;
   use rp_pico::hal::{self, Sio};
   use rp_pico::XOSC_CRYSTAL_FREQ;
 
-use crate::consts::TIMER_FREQUENCY;
-
-
-  const SCAN_TIME_US: u32 = 1000000;
+  use crate::consts::{self, GPIO_CONTINUOUS_ACTUATION_ACTIVE_POLARITY, GPIO_SHORT_ACTUATION_PERIOD, TIMER_FREQUENCY};
 
   #[shared]
   struct Shared {
     timer: hal::Timer,
     alarm: hal::timer::Alarm0,
-    led: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio25, hal::gpio::PushPullOutput>,
     interrupt_pin: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio20, hal::gpio::PullDownInput>,
+    continuous_pin: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio19, hal::gpio::PushPullOutput>,
+    short_pin: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio18, hal::gpio::PushPullOutput>,
   }
 
   #[local]
   struct Local {
-    delay: Delay
+    led: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio25, hal::gpio::PushPullOutput>,
   }
 
   #[init]
   fn init(mut c: init::Context) -> (Shared, Local, init::Monotonics) {
-    info!("Initializing rpi");
+    info!("Initializing Raspberry Pi Pico.");
     // Soft-reset does not release the hardware spinlocks
     // Release them now to avoid a deadlock after debug or watchdog reset
     unsafe {
       hal::sio::spinlock_reset();
     }
+
     let mut resets = c.device.RESETS;
     let mut watchdog = Watchdog::new(c.device.WATCHDOG);
-    let clocks = init_clocks_and_plls(
+    let _clocks = init_clocks_and_plls(
       XOSC_CRYSTAL_FREQ,
       c.device.XOSC,
       c.device.CLOCKS,
@@ -60,23 +58,25 @@ use crate::consts::TIMER_FREQUENCY;
 
     let sio = Sio::new(c.device.SIO);
     let pins = rp_pico::Pins::new(c.device.IO_BANK0, c.device.PADS_BANK0, sio.gpio_bank0, &mut resets);
+
     let mut led = pins.led.into_push_pull_output();
-    led.set_high().unwrap();
+    if cfg!(feature = "wifi") {
+      led.set_high().unwrap();
+    }
 
     let interrupt_pin = pins.gpio20.into_pull_down_input();
     interrupt_pin.set_interrupt_enabled(hal::gpio::Interrupt::EdgeHigh, true);
 
-    unsafe { rp_pico::pac::NVIC::unmask(rp_pico::pac::Interrupt::IO_IRQ_BANK0) }
+    let short_pin = pins.gpio18.into_push_pull_output();
+    let continuous_pin = pins.gpio19.into_push_pull_output();
 
     let mut timer = hal::Timer::new(c.device.TIMER, &mut resets);
     let mut alarm = timer.alarm_0().unwrap();
-    // let _ = alarm.schedule(SCAN_TIME_US.microseconds());
     alarm.enable_interrupt();
 
-    let delay = Delay::new(c.core.SYST, TIMER_FREQUENCY as u32);
-
+    // Configure cores
     c.core.SCB.set_sleepdeep();
-
+    unsafe { rp_pico::pac::NVIC::unmask(rp_pico::pac::Interrupt::IO_IRQ_BANK0) }
 
     info!("Initialization successful, delegating control flow to rtic");
 
@@ -84,10 +84,11 @@ use crate::consts::TIMER_FREQUENCY;
       Shared {
         timer,
         alarm,
-        led,
         interrupt_pin,
+        continuous_pin,
+        short_pin,
       },
-      Local {delay},
+      Local { led },
       init::Monotonics(),
     )
   }
@@ -95,13 +96,12 @@ use crate::consts::TIMER_FREQUENCY;
   #[task(
         binds = TIMER_IRQ_0,
         priority = 2,
-        shared = [timer, alarm, led],
+        shared = [timer, alarm],
     )]
   fn timer_irq(mut c: timer_irq::Context) {
     info!("From Timer interrupt.");
 
-    (&mut c.shared.led, &mut c.shared.alarm).lock(|l, alarm| {
-      l.set_low().unwrap();
+    c.shared.alarm.lock(|alarm| {
       alarm.clear_interrupt();
     });
   }
@@ -109,35 +109,59 @@ use crate::consts::TIMER_FREQUENCY;
   #[task(
         binds = IO_IRQ_BANK0,
         priority = 1,
-        shared = [alarm, led, interrupt_pin, timer],
-        local = [led_active: PinState = PinState::Low, last_value: u64 = 0]
+        shared = [alarm, interrupt_pin, timer, short_pin, continuous_pin],
+        local = [power: bool = false, last_value: u64 = 0, led]
     )]
   fn gpio_irq(mut c: gpio_irq::Context) {
-    debug!("Entering GPIO IRQ with local state: {}", *c.local.last_value);
-    *c.local.led_active = !*c.local.led_active;
+    debug!(
+      "Entering GPIO IRQ with local state: {}, {}",
+      *c.local.last_value, *c.local.power
+    );
+
+    // toggle state
+    *c.local.power = !*c.local.power;
+
+    let (continuous_power, short_power) = match *c.local.power {
+      true => (
+        GPIO_CONTINUOUS_ACTUATION_ACTIVE_POLARITY,
+        consts::GPIO_SHORT_ACTUATION_ACTIVE_POLARITY,
+      ),
+      false => (
+        !GPIO_CONTINUOUS_ACTUATION_ACTIVE_POLARITY,
+        !consts::GPIO_SHORT_ACTUATION_ACTIVE_POLARITY,
+      ),
+    };
 
     // write state using single lock
     (
-      &mut c.shared.led,
       &mut c.shared.interrupt_pin,
+      &mut c.shared.short_pin,
+      &mut c.shared.continuous_pin,
       &mut c.shared.alarm,
       &mut c.shared.timer,
     )
-      .lock(|led, interrupt_pin, alarm, timer| {
-        if (*c.local.last_value + TIMER_FREQUENCY ) < timer.get_counter() {
+      .lock(|ip, sp, cp, alarm, timer| {
+        if (*c.local.last_value + TIMER_FREQUENCY) < timer.get_counter() {
+          cp.set_state(continuous_power).unwrap();
+          sp.set_state(short_power).unwrap();
+
+          if cfg!(not(feature = "wifi")) {
+            c.local.led.set_state((*c.local.power).into()).unwrap();
+          }
+
           *c.local.last_value = timer.get_counter();
-          led.set_state(*c.local.led_active).unwrap();
-          alarm.schedule(SCAN_TIME_US.microseconds()).unwrap();
+          alarm.schedule(GPIO_SHORT_ACTUATION_PERIOD.microseconds()).unwrap();
+
           info!("Button press detected.");
         }
-        interrupt_pin.clear_interrupt(hal::gpio::Interrupt::EdgeHigh);
+        ip.clear_interrupt(hal::gpio::Interrupt::EdgeHigh);
       });
   }
 
-  #[idle(shared = [timer], local = [delay])]
+  #[idle(shared = [timer])]
   fn idle(_c: idle::Context) -> ! {
     loop {
-     cortex_m::asm::nop();
-     }
+      cortex_m::asm::nop();
+    }
   }
 }
